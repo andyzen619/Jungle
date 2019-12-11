a8beac0b750fb5738552ac9a3e52247a021b105d
andyzen619 <andyzen619@gmail.com>
2019-12-11 21:48:09 +0000

    Add dashboard information for numbe of products and categories in database

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @products = Product.order(id: :desc).all
+    @categories = Category.all
+
+    puts "Number of products in database: #{@products.length}"
+    puts "Number of categories in database: #{@categories.length}"
+
+    render "show"
+
   end
 end
