42face7ee2e872f057690d6ff65f191eb90f355a
andyzen619 <andyzen619@gmail.com>
2019-12-12 19:11:44 +0000

    Add Category option in dropdown for admin

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,20 +1,6 @@
 class CategoriesController < ApplicationController
-  
-  def index
-
-  end
-
-  def create
-
-  end
-
-  def new
-
-  end
-
   def show
     @category = Category.find(params[:id])
     @products = @category.products.order(created_at: :desc)
   end
-
 end
