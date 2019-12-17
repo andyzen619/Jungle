5b75aa53cd0a45a54c8ebb9b92679caf3d3bc7e7
andyzen619 <andyzen619@gmail.com>
2019-12-17 18:18:27 +0000

    Add rspec test home to product page navigation

--- spec/features/product_details_spec.rb
+++ spec/features/product_details_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-RSpec.feature "ProductDetails", type: :feature do
+RSpec.feature "ProductDetails", type: :feature, js: true do
   
   # SETUP
   before :each do
@@ -15,5 +15,16 @@ RSpec.feature "ProductDetails", type: :feature do
         image: open_asset('apparel1.jpg'),
       )
     end
+  end
+
+  scenario "They see product details page" do
+    visit root_path
+
+    first('.actions').click_on('Details')
+
+    # # DEBUG / VERIFY
+    # save_screenshot
+
+    expect(page).to have_content("Description")
   end
 end
